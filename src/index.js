7a9dd808c8dbfb3f8ef956c03dac43031d46bab0
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-04-17 12:44:22 -0400

    color de fondo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Layout from './layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
+import './index.css';
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
