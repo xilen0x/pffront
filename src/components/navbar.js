6efeec95159412fbbced27f332cc39b544120e97
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-04-20 22:10:30 -0400

    Trabajando en modal login y register

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = props => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <a className="navbar-brand" href="/">NOMBRE</a>
+                <Link className="navbar-brand" to="/">NOMBRE</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav ml-auto">
-                        <a className="nav-item nav-link active" href="/quienessomos">Quienes Somos<span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link" href="/blogs">Blog</a>
-                        <a className="nav-item nav-link" href="/ourwork">Nuestros Tramites</a>
-                        <a className="nav-item nav-link" href="pricing">Precios</a>
-                        <a className="nav-item nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">Entrar</a>
-                        <a className="nav-item nav-link" href="/register">Registrate</a>
+                        <Link className="nav-item nav-link active" to="/quienessomos">Quienes Somos<span className="sr-only">(current)</span></Link>
+                        <Link className="nav-item nav-link" to="/blogs">Blog</Link>
+                        <Link className="nav-item nav-link" to="/ourwork">Nuestros Tramites</Link>
+                        <Link className="nav-item nav-link" to="pricing">Precios</Link>
+                        <Link className="nav-item nav-link" to="#" data-toggle="modal" data-target="#ModalLogin">Entrar</Link>
+                        <Link className="nav-item nav-link" to="#" data-toggle="modal" data-target="#ModalRegister">Regístrate</Link>
                     </div>
                 </div>
             </nav>
