ae56cb40789dca35c9b494a960795a9eda04864a
jose armando carballo <jacl12592@gmail.com>
2020-04-21 10:28:40 -0400

    visual home

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,8 @@
 import React, { useContext, useState } from 'react';
 import Carousel from '../components/carousel';
+import Coment from '../components/coment';
+import Cards from '../components/cards';
+import News from '../components/news';
 
 const Home = props => {
     const [state, setState] = useState({
@@ -22,13 +25,37 @@ const Home = props => {
                 title: "Desierto de Atacama, Norte de Chile",
                 description: "Uno de los lugares más aridos del planeta."
             }
+        ],
+        blog:[
+            {
+                id: 1,
+                image: 'https://miviaje.com/wp-content/uploads/2017/11/laguna-miscanti-san-pedro-atacama.jpg',
+                title: "Tramite 1",
+                description: "contenido descriptivo de tramite" 
+            },
+            {
+                id: 2,
+                image: 'direcion de imagen',
+                title: "Tramite 2",
+                description: "contenido descriptivo de tramite" 
+            },
+            {
+                id: 3,
+                image: 'direcion de imagen',
+                title: "Tramite 3",
+                description: "contenido descriptivo de tramite" 
+            }
         ]
     })
 
     return (
         <>
             <Carousel slide={state.galery} />
-            <h1>Home</h1>
+            <h1 className= "title text-center">Home</h1>
+            <Cards conte={state.blog} />
+            <Coment conte={state.blog} />
+            <News conte={state.blog}/>
+            
         </>
     )
 }
